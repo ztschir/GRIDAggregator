94e3e6ad7e332b2816150ca7e2eb3dc123b15b0e
Zachary Tschirhart <ztschir@gmail.com>
2012-07-26 01:22:50 -0500

    Pushing latest changes and trying Twitter Integration

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,22 +4,24 @@ class PagesController < ApplicationController
     @title = "Home"
     
     @h = LazyHighCharts::HighChart.new('graph') do |f|
-
-      #channelData = Channel.joins(:offset_time).joins(:signal_type).find(:all,
-      #                :conditions => "transmitter_id = 1 AND signal_types.name = 'GPS_L1_CA'",
-      #                :order => "utc_time ASC")
+            
+            
+      @receiverIDs = Receiver.find(:all, :select => 'id, name', :order => 'id')
+     # @transmitterIDs = Transmitter.find(:all, :unique, :select => 'transmitter_id', :order => 'transmitter_id')
                       
       channelData1 = Channel.joins(:offset_time).joins(:signal_type).find(:all, 
                       :select => 'pseudorange, offset_times.utc_time',
-                      :conditions => "transmitter_id = 1 AND signal_types.name = 'GPS_L1_CA'",
+                      :conditions => "transmitter_id = 1 AND signal_types.name = 'GPS_L1_CA' AND receiver_id = 19",
                       :order => "offset_times.utc_time ASC").collect{|query| [query["utc_time"].to_datetime.to_i * 1000, query["pseudorange"]]}
           
           
-      channelData2 = Channel.joins(:offset_time).joins(:signal_type).find(:all, 
-                      :select => 'pseudorange, offset_times.utc_time',
-                      :conditions => "transmitter_id = 2 AND signal_types.name = 'GPS_L1_CA'",
-                      :order => "offset_times.utc_time ASC").collect{|query| [query["utc_time"].to_datetime.to_i * 1000, query["pseudorange"]]}
+      #channelData2 = Channel.joins(:offset_time).joins(:signal_type).find(:all, 
+      #                :select => 'pseudorange, offset_times.utc_time',
+      #                :conditions => "transmitter_id = 2 AND signal_types.name = 'GPS_L1_CA' AND receiver_id = 19",
+      #                :order => "offset_times.utc_time ASC").collect{|query| [query["utc_time"].to_datetime.to_i * 1000, query["pseudorange"]]}
 
+
+      
       #@channelDates = Array.new
       #channelData.each do |dates|
       #    stuff = Array.new
@@ -30,9 +32,10 @@ class PagesController < ApplicationController
 
       f.options[:chart][:defaultSeriesType] = "area"
       f.options[:xAxis][:type] = "datetime"
+      f.options[:legend][:enabled] = true
       
       f.series(:name=>'Transmitter 1', :data=> channelData1)
-      f.series(:name=>'Transmitter 2', :data=> channelData2)
+      #f.series(:name=>'Transmitter 2', :data=> channelData2)
       
     end
     
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -3,4 +3,5 @@ class Channel < ActiveRecord::Base
   belongs_to :offset_time
   belongs_to :channel_status
   belongs_to :signal_type
+  belongs_to :receiver
 end
