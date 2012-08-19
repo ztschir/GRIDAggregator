15f5a10b71efa4438dc95fc39af66fb4a3610bb6
Zachary Tschirhart <ztschir@gmail.com>
2012-08-19 00:22:46 -0500

    Added tags

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,13 +7,21 @@ class PagesController < ApplicationController
             
             
       @receiverIDs = Receiver.find(:all, :select => 'id, name', :order => 'id')
-     # @transmitterIDs = Transmitter.find(:all, :unique, :select => 'transmitter_id', :order => 'transmitter_id')
+      @transmitterIDs = Transmitter.find(:all, 
+                                         :select => 'distinct transmitter_id',
+                                         :conditions => 'receiver_id = 19', 
+                                         :order => 'transmitter_id')
+      @signalTypes = SignalType.find(:all,
+                                      :select => 'name',
+                                      :order => 'name')
                       
-      channelData1 = Channel.joins(:offset_time).joins(:signal_type).find(:all, 
+      channelData1 = Channel.joins(:signal_type).joins(:offset_time).find(:all, 
+                      #:joins => 'LEFT JOIN offset_times on offset_times.id = channels.offset_time_id',
                       :select => 'pseudorange, offset_times.utc_time',
                       :conditions => "transmitter_id = 1 AND signal_types.name = 'GPS_L1_CA' AND receiver_id = 19",
-                      :order => "offset_times.utc_time ASC").collect{|query| [query["utc_time"].to_datetime.to_i * 1000, query["pseudorange"]]}
-          
+                      :order => "offset_times.utc_time ASC")
+                      .collect{|query| [query["utc_time"].to_datetime.to_i * 1000, query["pseudorange"]]}
+      #add_missing_dates(channelData1)
           
       #channelData2 = Channel.joins(:offset_time).joins(:signal_type).find(:all, 
       #                :select => 'pseudorange, offset_times.utc_time',
@@ -40,6 +48,27 @@ class PagesController < ApplicationController
     end
     
   end
+  
+  require 'date'
+  require 'pp'
+
+  def add_missing_dates(series)
+    series.map do |date, value|
+      [date, value]
+    end.inject([]) do |series, date_and_value|
+      filler = if series.empty?
+                 []
+               else
+                 ((series.last[0]+ 1)..(date_and_value[0] - 1)).map do |date|
+                   [date, 0]
+                 end
+               end
+      series + filler + [date_and_value]
+    end.map do |date, value|
+      [date, value]
+    end
+  end
+  
 
   def team
     @title = "Team"
